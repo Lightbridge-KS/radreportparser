006372c09a4faf810e6a46278d1a44d9999f6664
Lightbridge-KS <ki11ip0.s.a.s@gmail.com>
2025-02-17 20:39:36 +0700

    Use `setuptools_scm` for versioning

--- radreportparser/__init__.py
+++ radreportparser/__init__.py
@@ -1,3 +1,9 @@
+"""radreportparser - Regex-based text parser for common radiology report"""
+
+# Change to import.metadata when minimum python>=3.8
+from importlib_metadata import version as _version
+
+
 from .extractor import (
     SectionConfig,
     RadReportExtractor,
@@ -7,3 +13,19 @@ from .report import RadReport
 from .section import (
     SectionExtractor
     )
+
+# __version__ = "0.1.0-alpha.1"
+
+
+__all__ = [
+    "SectionConfig",
+    "RadReportExtractor",
+    "KeyWord",
+    "RadReport",
+    "SectionExtractor",
+    "__version__",
+]
+
+__author__ = "Kittipos Sirivongrungson <ki11ip0.s.a.s@gmail.com>"
+__version__ = _version("radreportparser")
+del _version
